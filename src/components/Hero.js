3dae7c9bd586d66dfea282f502918e5d40aca204
NurAhissami <nurahi10@gmail.com>
2021-07-14 22:38:27 +0200

    Add part of desktop design

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,17 +4,19 @@ function Hero() {
   return (
     <section className="hero">
       <div className="hero__bg"></div>
-      <div className="hero__title">
-        <h3 className="hero__hello">Hola Mundo!</h3>
-        <div className="hero__names">
-          <span className="hero__names--me"> Soy</span>
-          <h1 className="hero__names--name">Nur</h1>
+      <div className="hero__container">
+        <div className="hero__title">
+          <h3 className="hero__hello">Hola Mundo!</h3>
+          <div className="hero__names">
+            <span className="hero__names--me"> Soy</span>
+            <h1 className="hero__names--name">Nur</h1>
+          </div>
         </div>
+        <p className="hero__text">
+          Soy desarrolladora FrontEnd y diseñadora Gráfica
+        </p>
+        <button className="hero__button">¡Conóceme!</button>
       </div>
-      <p className="hero__text">
-        Soy desarrolladora FrontEnd y diseñadora Gráfica
-      </p>
-      <button className="hero__button">¡Conóceme!</button>
       <div className="hero__circle">
         <i className="hero__circle--arrow fas fa-arrow-down"></i>
       </div>
